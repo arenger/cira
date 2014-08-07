92025bbf790d56ad10d80c3acdd79c480af5bff0
Alex Renger <arenger@gmail.com>
2014-08-07 12:40:05 -0400

    started parse code

--- src/main/java/com/example/CiraCsv.java
+++ src/main/java/com/example/CiraCsv.java
@@ -4,6 +4,8 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.math.BigDecimal;
+import java.text.ParseException;
+import java.util.ArrayList;
 import java.util.List;
 
 import au.com.bytecode.opencsv.CSVReader;
@@ -13,27 +15,60 @@ public class CiraCsv {
    private List<CiraRec> items;
    private BigDecimal totalAmount;
 
-   public CiraCsv(String filename) throws IOException {
+   public CiraCsv(String filename) throws IOException, ParseException {
       init(new File(filename));
    }
 
-   public CiraCsv(File file) throws IOException {
+   public CiraCsv(File file) throws IOException, ParseException {
       init(file);
    }
 
-   private void init(File file) throws IOException {
+   private void assertFieldCount(String[] fields, int count, int lineNum) 
+      throws ParseException {
+      if (fields.length != count) {
+         throw new ParseException(String.format(
+            "Expected %d fields on line %d. Found %d",
+            fields.length, lineNum, count), lineNum);
+      }
+   }
+
+   private void init(File file) throws IOException, ParseException {
       CSVReader reader = null;
+      int lineNum  = 1; //used for error reporting
+      int fieldNum = 1; //used for error reporting
       try {
          reader = new CSVReader(new FileReader(file));
-         String[] nextLine;
-         while ((nextLine = reader.readNext()) != null) {
-            System.out.println(nextLine[0] + nextLine[1] + "etc...");
+         items = new ArrayList<CiraRec>();
+         totalAmount = new BigDecimal(0);
+         int expectedItemCount = -1;
+         BigDecimal expectedTotalAmount = null;
+         String[] fields;
+         while ((fields = reader.readNext()) != null) {
+            if (lineNum == 3) {
+               assertFieldCount(fields, 2, lineNum);
+               fieldNum = 2;
+               expectedTotalAmount = new BigDecimal(fields[1]);
+            }
+            if (lineNum == 4) {
+               assertFieldCount(fields, 2, lineNum);
+               fieldNum = 2;
+               expectedItemCount = Integer.parseInt(fields[1]);
+            }
+            lineNum++;
          }
+      } catch (NumberFormatException e) {
+         throw new ParseException(String.format(
+            "NumberFormatException while parsing field %d on line %d",
+            fieldNum, lineNum), lineNum);
       } finally {
          if (reader != null) {
             reader.close();
          }
       }
+   }
+
+   public static void main(String[] args) throws Exception {
+      CiraCsv cc = new CiraCsv(args[0]);
    }
 
    public CiraRec getItem(int index) {
